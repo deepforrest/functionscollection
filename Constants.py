d922d923ada593823fc7198e93ea7be090a69b29
Forrest Baird <forrestmsbaird@gmail.com>
2022-05-16 12:40:24 -0700

    More and More!

---
+++ Constants.py
@@ -0,0 +1,7 @@
+# Physics Constants
+
+iGravityConstant = 6.67 * (10 ** -11) # [m][m][s] / [kg][kg][kg]
+
+iRadiusOfEarth = 6.37 * (10 ** 6)     # [m]
+iMassOfEarth = 5.98 * (10 ** 24)      # [kg]
+iAccelGravEarth = (iGravityConstant * iMassOfEarth) / (iRadiusOfEarth ** 2)
--- Mechanics.py
+++ Mechanics.py
@@ -1,11 +1,11 @@
-def iPositionFinal(iPositionInit, iVelocity, iTime):
+def iPositionFinal(iPosInit, iVel, iAccel, iTime):
 
-    return iPositionInit + iVelocity * iTime
+    return iPosInit + iVel * iTime + 1 / 2 * iAccel * (iTime ** 2)
 
 
-def iPositionInit(iPositionFinal, iVelocity, iTime):
+def iPositionInit(iPosFinal, iVel, iTime):
 
-    return iPositionFinal - iVelocity * iTime
+    return iPosFinal - (iVel * iTime + 1 / 2 * iAccel * (iTime ** 2))
 
 
 def iVelocityAvg(iPosFinal, iPosInit, iTimeFinal, iTimeInit):
@@ -13,9 +13,9 @@ def iVelocityAvg(iPosFinal, iPosInit, iTimeFinal, iTimeInit):
     return (iPosFinal - iPosInit) / (iTimeFinal - iTimeInit)
 
 
-def iVelocityFinal(iVelInit, iAccel, iTime):
+def iVelocityFinal(iVelInit, iAccel, iDeltaTime):
 
-    return iVelInit + iAccel * iTime
+    return iVelInit + iAccel * iDeltaTime
 
 
 def iAccelerationAvg(iVelFinal, iVelInit, iTimeFinal, iTimeInit):
@@ -23,9 +23,31 @@ def iAccelerationAvg(iVelFinal, iVelInit, iTimeFinal, iTimeInit):
     return (iVelFinal - iVelInit) / (iTimeFinal - iTimeInit)
 
 
+def iAccelerationCentripetal(iVel, iRadius)
+
+    return (iVel ** 2) / iRadius
+
+def iCircularMotionPeriod(iRadius, iVelocity)
+
+    return (2 * math.pi() * iRadius) / iVelocity
+
+
 def iForce(iMass, iAcceleration):
 
     return iMass * iAcceleration
+
+
+# Newton's Third Law
+def iReactiveForce(iForce):
+
+    return -iForce
+
+def iForceNewtonLawofGravity(iMass_1, iMass_2, iDistance)
+
+    return iGravityConstant * (iMass_1 * iMass_2) / (iDistance ** 2)
+
+
+
 
 
 def iMomentum(iMass, iVelocity):
