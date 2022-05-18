0bf172f42b08f80266ebd9f217f36cb09d2ae93c
Forrest Baird <forrestmsbaird@gmail.com>
2022-05-18 10:39:39 -0700

    Few more math functions

--- MathFunctions.py
+++ MathFunctions.py
@@ -33,4 +33,54 @@ def iDifferentiatePolyTerm(iCoeff, iVar, iPower):
 
     return (iCoeff * iPower * (iVar ** iNewPower))
 
+# Tested.  Returns an array of prime numbers that when multiplied together, create the original number.  Optimized for speed
+def iArrFactorsOfNum(iNum):
 
+    iArrFactors = []
+    iDivisor = 2
+
+    while iDivisor <= iNum:
+
+        #print ("Number = " + str(iNum))
+        #print ("Divisor = " + str(iDivisor))
+
+        while iNum % iDivisor == 0:
+
+            iArrFactors.append(iDivisor)
+            #print(str(iNum) + " is divisible by " + str(iDivisor))
+
+            iNum /= iDivisor
+            #print("New Number: " + str(iNum))
+
+        if iDivisor != 2:
+
+            iDivisor += 2
+
+        else:
+
+            iDivisor += 1
+
+    if len(iArrFactors) == 1:
+
+        return "1 and itself"
+
+    return iArrFactors
+
+def bIsPrimeNumber(iNum):
+
+    iMidPoint = iNum // 2
+    iDivisor = 2
+
+    while iNum <= iMidPoint:
+
+        if iNum % iDivisor == 0: return False
+
+        if iDivisor != 2:
+
+            iDivisor += 2
+
+        else:
+
+            iDivisor += 1
+
+    return True
