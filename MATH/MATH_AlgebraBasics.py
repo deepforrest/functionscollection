8d1fd746766ebe7b12d14f1722734bd98cac508a
Forrest Baird <forrestmsbaird@gmail.com>
2022-05-27 21:46:53 -0700

    Factoring is alot easier on paper than with code

--- MATH/MATH_AlgebraBasics.py
+++ MATH/MATH_AlgebraBasics.py
@@ -50,16 +50,25 @@ def sFOILExpression(iA_1, iA_2, iB_1, iB_2):
 
 def sFactorTrinomial(iArrTrinomialCoeff, bDebug = False):
 
-    if len(iArrTrinomialCoeff) != 3: return "Not a trinomial set of coefficients!"
+    if len(iArrTrinomialCoeff) != 3: 
+        return "Not a trinomial set of coefficients!"
+
+    for iCoeff in iArrTrinomialCoeff:
+
+        if type(iArrTrinomialCoeff[iCoeff]) != int:
+
+            return "Noninteger detected!"
 
     bDebug and print("{} x^2 + {} x + {}".format(iArrTrinomialCoeff[0], iArrTrinomialCoeff[1], iArrTrinomialCoeff[2]))
 
-    iA_MaxTerm = iArrTrinomialCoeff[0]
-    iB_MaxTerm = iArrTrinomialCoeff[2]
+    iQuadTerm = iArrTrinomialCoeff[0]
+    iLinearTerm = iArrTrinomialCoeff[1]
+    iConstTerm = iArrTrinomialCoeff[2]
 
-    iA_MP = round(iSqrt(iA_MaxTerm), 0)
-    iB_MP = round(iSqrt(iB_MaxTerm), 0)
-
+    # Midpoints
+    iA_MP = round(iSqrt(iQuadTerm), 0)
+    iB_MP = round(iSqrt(iConstTerm), 0)
+    
     # Baseline Points to Start The Factoring Process.
     iA_1 = iA_MP
     iA_2 = iA_MP
@@ -67,8 +76,29 @@ def sFactorTrinomial(iArrTrinomialCoeff, bDebug = False):
     iB_1 = iB_MP
     iB_2 = iB_MP
 
-    iA_MP = round(iSqrt(iA_MaxTerm), 0)
-    iB_MP = round(iSqrt(iB_MaxTerm), 0)
+    iMP_Smallest = min(iA_MP, iB_MP)
 
-    
-     
+    while iMP_Smallest > 0:
+
+        # Initialization Conditions
+
+        while iB_2 <= iConstTerm:
+
+            iTestQuadTerm = iA_1 * iA_2
+            iTestLinearTerm = iA_1 * iB_2 + iA_2 * iB_1
+            iTestConstTerm = iB_1 * iB_2
+
+            iArrTestCoeff = [iTestQuadTerm, iTestLinearTerm, iTestConstTerm]
+
+            if iArrTestCoeff = iArrTrinomialCoeff:
+
+                sLHS = "{} x^2 + {} x + {}".format(iTestQuadTerm, iTestLinearTerm, iTestConstTerm)
+                sRHS = "({}x + {})({}x + {}".format(iA_1, iA_2, iB_1, iB_2)
+
+                print(sLHS + " = " sRHS)
+                return
+
+            iB_2 += 1
+
+        iMP_Smallest -= 1
+        
